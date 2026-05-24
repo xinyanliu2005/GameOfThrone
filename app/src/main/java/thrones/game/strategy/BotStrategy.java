8c68e4cde31ec4de0b48f23a626cb6ef9f16622c
GyDeen <zde421217757@outlook.com>
2026-05-24 14:50:13 +1000

    Update commenting

--- app/src/main/java/thrones/game/strategy/BotStrategy.java
+++ app/src/main/java/thrones/game/strategy/BotStrategy.java
@@ -7,7 +7,8 @@ import java.util.Optional;
 
 public interface BotStrategy {
     /**
-     * Evaluates selected card.
+     * Evaluates selected card. If it doesn't fall into the consideration, return false.
+     * If it violate the consideration, return false.
      * @return whether we can play the card.
      */
     public boolean isSafeToPlay(Card selectedCard, int targetPileIndex, PileInformation boardInfo, int playerIdentifier);
--- app/src/main/java/thrones/game/strategy/CompositeStrategy.java
+++ app/src/main/java/thrones/game/strategy/CompositeStrategy.java
@@ -11,10 +11,15 @@ import java.util.Optional;
 public class CompositeStrategy implements BotStrategy {
     private final List<BotStrategy> STRATEGIES = new ArrayList<>();
 
+    /**
+     * Adds a strategy to the composite strategy.
+     */
     public void addStrategy(BotStrategy strategy) {
         this.STRATEGIES.add(strategy);
     }
 
+
+    /** Determine whether we can perform the given move based on pre-configed strategies. */
     public Optional<BotMove> determineMove(Card selectedCard, int targetPileIndex, PileInformation boardInfo, int playerIdentifier) {
         if (STRATEGIES.isEmpty()) {
             return Optional.empty();
@@ -28,6 +33,7 @@ public class CompositeStrategy implements BotStrategy {
         return Optional.empty();
     }
 
+    /** */
     @Override
     public boolean isSafeToPlay(Card selectedCard, int targetPileIndex, PileInformation boardInfo, int playerIdentifier) {
         return determineMove(selectedCard, targetPileIndex, boardInfo, playerIdentifier).isPresent();
